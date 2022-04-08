e6118eaf74d8f86321e5558fa7cd32f8e702871b
YangWei <961433139@qq.com>
2022-04-08 16:40:41 +0800

    添加全局单例对象，移除Mvvm项目

---
+++ IDCA.Client/Singleton/GlobalConfig.cs
@@ -0,0 +1,27 @@
+﻿using IDCA.Bll.Template;
+
+namespace IDCA.Client.Singleton
+{
+    public class GlobalConfig
+    {
+        public GlobalConfig() { }
+
+        private readonly static GlobalConfig _instance = new();
+        public static GlobalConfig Instance => _instance;
+
+
+        string _projectRootPath = string.Empty;
+        /// <summary>
+        /// 获取或配置当前配置中的项目根目录
+        /// </summary>
+        public string ProjectRootPath { get => _projectRootPath; set => _projectRootPath = value; }
+
+        TemplateCollection? _templates;
+        /// <summary>
+        /// 获取或配置当前项目的模板类型，未配置时值为null
+        /// </summary>
+        public TemplateCollection? Templates { get => _templates; set => _templates = value; }
+
+
+    }
+}
