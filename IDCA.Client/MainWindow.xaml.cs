d51eef85a75da2e32ebfa781bb314f7d3808210a
YangWei <961433139@qq.com>
2022-04-10 21:52:16 +0800

    修改UI

--- IDCA.Client/MainWindow.xaml.cs
+++ IDCA.Client/MainWindow.xaml.cs
@@ -14,48 +14,5 @@ namespace IDCA.Client
             InitializeComponent();
         }
 
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            switch (e.ClickCount)
-            {
-                case 1:
-                    try
-                    {
-                        DragMove();
-                    }
-                    catch
-                    {
-                    }
-                    break;
-                case 2:
-                    WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
-                    break;
-                default:
-                    break;
-            }
-
-        }
-
-        private void Window_Closing(object sender, CancelEventArgs e)
-        {
-            Application.Current.Shutdown();
-        }
-
-        private void CloseButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
-
-        private void MinimizeButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            WindowState = WindowState.Minimized;
-        }
-
-        private void MaximizeButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            WindowState = WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
-        }
-        
-
     }
 }
---
+++ IDCA.Client/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace IDCA.Client.ViewModel
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        public MainWindowViewModel()
+        {
+        }
+
+        string _projectPath = string.Empty;
+        /// <summary>
+        /// 当前项目的根目录完整路径
+        /// </summary>
+        public string ProjectPath
+        {
+            get { return _projectPath; }
+            set { SetProperty(ref _projectPath, value); }
+        }
+
+
+    }
+}
